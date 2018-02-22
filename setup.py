eaf3376d741af37d2c7f745759fc0f5ff2afb56b
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-02-22 17:42:22 +0000

    Third commit

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@
 import sys
 from setuptools import setup
 
-entry_points =[console_scripts]
-script_name = systeminfo.main:run
+entry_points = {'console_scripts': ['comp_30670_systeminfo=systeminfo.main:main']}
 
 
 
@@ -20,7 +19,7 @@ def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
     setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-          entry_points= {'console_scripts':['comp_30670_systeminfo=systeminfo.main:main']},
+          entry_points= entry_points,
           use_pyscaffold=True)
 
 
